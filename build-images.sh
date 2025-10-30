d7ff0494d94e442375242da20968c671a1f84e82
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-10-30 17:41:24 +0100

    Add min-core label to buildah config

--- build-images.sh
+++ build-images.sh
@@ -39,6 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
+    --label="org.nethserver.min-core=3.12.4-0" \
     --label="org.nethserver.images=docker.io/mariadb:10.11.14 docker.io/nginx:1.28.0-alpine" \
     "${container}"
 # Commit the image
